b9409cadf41599c3a22fb589edf5458f9f610b2f
vutr <me@vutr.io>
2020-08-24 20:41:39 +0700

    change to org

--- 01__init.sql
+++ 01__init.sql
@@ -1,3 +1,6 @@
+-- must be superuser to run the below command
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
+
 CREATE TABLE IF NOT EXISTS users
 (
   id SERIAL PRIMARY KEY,
--- readme.md
+++
@@ -1,9 +0,0 @@
-# Migration scripts
-
-1. `01__init`:
-  - create **user** table
-
-
-
-## Change log
-to be written
